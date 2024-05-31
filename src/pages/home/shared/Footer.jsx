c4216b727a73e356ebccca48083650d68e05b734
samwaseee <samwaseee@gmail.com>
2024-05-31 23:50:21 +0600

    Next ui and react awsome slider installed

---
+++ src/pages/home/shared/Footer.jsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content font-taj">
+            <aside>
+                <img src="/public/health-podcast.png" alt="" className="w-12"/>
+                <p> <span className="text-xl font-bold font-mon">Care Camp</span> <br />Organizing Medical Campaigns since 2014</p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
+const {nextui} = require('@nextui-org/theme');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
   ],
   theme: {
     extend: {},
     colors: {
       'blood': '#f5333f',
-      'secondary': '#ebebeb'
+      'second': '#ebebeb'
+    },
+    fontFamily: {
+        'taj': ['Tajawal', 'sans-serif'],
+        'mon': ['Montserrat', 'sans-serif']
     }
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui"),nextui()],
 }
