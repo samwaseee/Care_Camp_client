211d0ae8dd1e341aaade8b890983c1ffc34c6111
samwaseee <samwaseee@gmail.com>
2024-06-03 00:41:20 +0600

    swal added with context api

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,70 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { GoogleAuthProvider } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+
+const GoogleProvider = new GoogleAuthProvider();
+
+
+const auth = getAuth(app);
+
+// eslint-disable-next-line react/prop-types
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
+
+    const GoogleLogin = () => {
+        return signInWithPopup(auth, GoogleProvider)
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        updateUserProfile,
+        GoogleLogin,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
