dcf2b1f8d1e1590b4b6df2f332dc4472f6d3ee1f
samwaseee <samwaseee@gmail.com>
2024-06-02 21:02:52 +0600

    react awsome reveal

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,16 +15,43 @@ const Home = () => {
             >
                 <div>
                     <img src="https://www.ifrc.org/sites/default/files/styles/hero_image_desktop_1280_x_600_/public/2021-05/p-FJI0614%201280x500.jpg?itok=y3WEJrXb" alt="Image 0" className='w-[100vw] h-1/2' />
-                    <p className='max-w-2xl absolute bottom-0 left-1/4 bg-second p-10'>
-                        The IFRC is committed to supporting humanitarian action that is as local as possible, as international as necessary. Our 191 National Red Cross and Red Crescent Societies are the lead actors in preparing for, responding to and helping communities recover from emergencies. In times of increased need, our global disaster response system effectively supports and coordinates their lifesaving work.</p>
+                    <div className='max-w-2xl grid absolute bottom-0 left-1/4 bg-second'>
+                        <div className="card">
+                            <div className="card-body">
+                                <p>The IFRC is committed to supporting humanitarian action that is as local as possible, as international as necessary. Our 191 National Red Cross and Red Crescent Societies are the lead actors in preparing for, responding to and helping communities recover from emergencies. In times of increased need, our global disaster response system effectively supports and coordinates their lifesaving work.</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn rounded-none">Read story...</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div>
                     <img src="https://www.ifrc.org/sites/default/files/styles/featured_doc_thumbnail_desktop_648_x_453_/public/2021-07/p-BGD2651_jpg%201280x853.jpg?itok=euAsjVEH" alt="Image 1" className='w-[100vw]' />
-                    <p className='max-w-2xl absolute bottom-0 left-1/4 bg-second p-10'>The IFRC’s Health and Care Framework sets out our contribution to healthier, more resilient communities and individuals. It presents the collective priorities and programming modalities that define our work in health and care. It illustrates a pathway for National Societies’ engagement with public health authorities in their auxiliary role around health and care. And it links the work of the IFRC in health and care to the global agenda of the Sustainable Development Goals (SDGs).</p>
+                    <div className='max-w-2xl grid absolute bottom-0 left-1/4 bg-second'>
+                        <div className="card">
+                            <div className="card-body">
+                                <p>IFRC’s Health and Care Framework sets out our contribution to healthier, more resilient communities and individuals. It presents the collective priorities and programming modalities that define our work in health and care. It illustrates a pathway for National Societies’ engagement with public health authorities in their auxiliary role around health and care. And it links the work of the IFRC in health and care to the global agenda of the Sustainable Development Goals (SDGs).</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn rounded-none">Read story...</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <img src="https://www.ifrc.org/sites/default/files/styles/hero_image_desktop_1280_x_600_/public/2021-05/p-syr1356%201280x500.jpg?itok=JzyCm6l9" alt="Image 2" className='w-[100vw]' />
-                    <p className='max-w-2xl absolute bottom-0 left-1/4 bg-second p-10'>The IFRC, with the support of partners, works at the community, national and global levels to reduce people’s risk of contracting non-communicable diseases. Our approach promotes healthy choices and behaviours at all stages of a person’s life.</p>
+                    <div className='max-w-2xl grid absolute bottom-0 left-1/4 bg-second'>
+                        <div className="card">
+                            <div className="card-body">
+                                <p>The IFRC, with the support of partners, works at the community, national and global levels to reduce people’s risk of contracting non-communicable diseases. Our approach promotes healthy choices and behaviours at all stages of a person’s life.</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn rounded-none">Read story...</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </AutoplaySlider>
         </>
